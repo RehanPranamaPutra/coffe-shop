f1b26edf597aa9349d261e00b4051f0fcd3d79d0
rehanpranama <rehanpranama6@gmail.com>
2025-10-06 14:43:48 +0700

    migration database dan model

---
+++ database/migrations/2025_10_06_071932_create_promos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained('menus');
+            $table->enum('jenis_promo',['persen','nominal']);
+            $table->decimal('nilai_diskon',12,2);
+            $table->dateTime('tanggal_mulai');
+            $table->dateTime('tanggal_selesai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promos');
+    }
+};
