77182b7404d42334b88d716f5fb4d992540705e9
rehanpranama <rehanpranama6@gmail.com>
2026-01-03 10:29:45 +0700

    update model

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
+    protected $table = 'categories';
     protected $fillable = ['nama_kategori'];
 
     public function menus()
     {
-        return $this->hasMany(Menu::class);
+        return $this->hasMany(Menu::class,'categories_id');
     }
 }
